29dd561b94b57193b8d4b9deff8a3f712105ba57
thegenuinegeek <thegenuinegeek@users.noreply.github.com>
2026-09-19 13:47:45 -0400

    Fix absolute paths for GitHub Pages subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://genuinegeekmedia.com',
+  base: '/3nails-garage-door',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
